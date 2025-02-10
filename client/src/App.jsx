8791d457b7a7f01de2bf4f8cf51ee7f08550b7d6
Ivy Nguyen <ivynguyen2990@gmail.com>
2025-02-10 14:14:40 -0500

    implemented frontend login and register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import "./App.css"
 import Home from './pages/Home/Home'
+import LogIn from './pages/LogIn/LogIn'
+import Register from './pages/Register/Register'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<></>}/>
-        <Route path="/register" element={<></>}/>
+        <Route path="/login" element={<LogIn/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/pages/Register/Register.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useNavigate, Link } from 'react-router-dom'
+import config from '../../config/config.js'
+import './Register.css'
+
+function Register() {
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+  
+    const handleRegister = (e) => {
+      e.preventDefault()
+  
+      // validate user input
+      if(password.length < 8){
+        setError("Password must be at least 8 characters")
+        return
+      }
+
+      if(password !== confirmPassword){
+        setError("Passwords do not match")
+        return
+      }
+
+      axios.post(`${config.BASE_URL}/api/auth/register`, {
+          username, email, password
+      })
+      .then((response) => {
+          if(response.data.success){
+              navigate("/login");
+          } else {
+              setError(response.data.message);
+          }
+      })
+      .catch((error) => setError(error.response.data.message))
+    }
+  
+    return (
+    <div className='register-page'>
+      <div className="register-container">
+        <h1>Sign Up</h1>
+        <form className="register-form" onSubmit={handleRegister}>
+            <div className='field'>
+                <label htmlFor="username">Username: </label>
+                <input 
+                    type="text" 
+                    id="username"
+                    placeholder="Enter your username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                /> 
+            </div>
+              
+            <div className='field'>
+                <label htmlFor="email">Email: </label>
+                <input 
+                    type="email" 
+                    id="email" 
+                    placeholder="Enter your email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                /> 
+            </div>
+              
+            <div className='field'>
+                <label htmlFor="password">Password: </label>
+                <input 
+                    type="password" 
+                    id="password"
+                    placeholder="Enter your password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </div>
+
+            <div className='field'>
+                <label htmlFor="confirm-password">Confirm Password: </label>
+                <input 
+                    type="password" 
+                    id="confirm-password"
+                    placeholder="Confirm your password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+            </div>
+            
+            {error ? <div className="error-message">{error}</div> : <></>}
+
+            <button type="submit">Sign Up</button>
+            <p>Already have an account?</p>
+            <Link to="/login"><div>Log In</div></Link>
+        </form>
+      </div>
+    </div>
+    )
+}
+
+export default Register
