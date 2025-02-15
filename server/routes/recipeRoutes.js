ba93bb3ab5ab7e599cea103c97df8356d899e854
Ivy Nguyen <ivynguyen2990@gmail.com>
2025-02-15 09:13:49 -0500

    fixed route endpoints

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -6,11 +6,11 @@ const recipeRouter = express.Router()
 
 recipeRouter.post("/", authenticateUser, recipeController.createRecipe);
 recipeRouter.get("/", recipeController.getAllRecipes);
-recipeRouter.get("/:id", recipeController.getRecipe);
-recipeRouter.put("/:id", authenticateUser, recipeController.editRecipe);
-recipeRouter.delete("/:id", authenticateUser, recipeController.deleteRecipe);
+recipeRouter.get("/recipe/:id", recipeController.getRecipe);
+recipeRouter.put("/recipe/:id", authenticateUser, recipeController.editRecipe);
+recipeRouter.delete("/recipe/:id", authenticateUser, recipeController.deleteRecipe);
 // Route needs to be "user/recipes/", doing "recipes/user/" will not work. Route will think "user" is a recipe id
-recipeRouter.get("/user/recipes/", authenticateUser, recipeController.getUserRecipes);
+recipeRouter.get("/user", authenticateUser, recipeController.getUserRecipes);
 
 
 
