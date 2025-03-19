f4586492dbd7effc58c8f6f2416778625a8c88da
Michael Wang <wangmichael2005@gmail.com>
2025-03-19 05:40:40 -0400

    fix: Edited model's system instructions to better fit role of cooking assistant.

--- server/controllers/aiAssistantController.js
+++ server/controllers/aiAssistantController.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: AI_ASSISTANT_MODEL });
+const model = genAI.getGenerativeModel({ model: AI_ASSISTANT_MODEL, systemInstruction: "You are a cooking helper/mentor. Don't answer anything unrelated."});
 
 const chatSessions = {};
 
