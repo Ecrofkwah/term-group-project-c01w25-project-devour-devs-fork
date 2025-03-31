550dd1b63cd5538b7fa186233a2f694d990a0152
dany-2314 <dshahid980@gmail.com>
2025-03-31 00:36:23 -0400

    fix: added some fixes for meal card

--- client/src/components/MealCard/MealCard.jsx
+++ client/src/components/MealCard/MealCard.jsx
@@ -4,6 +4,9 @@ import './MealCard.css'
 import axios from 'axios'
 import config from '../../config/config';
 import { Rating } from '../MealRate/rating.jsx'
+import Card from 'react-bootstrap/Card';
+import { CardBody } from 'react-bootstrap';
+
 function MealCard({meal}) {
   const navigate = useNavigate()
   const [rating, setRating] = useState(null);
@@ -29,15 +32,45 @@ function MealCard({meal}) {
   }, []);
   
   return (
-    <div className='meal-card' onClick={handleCardClick}>
-      <img src={meal.image}/>
-      <div className='meal-card-content'>
-        <div className='title'>{meal.title}</div>
-        <div>Cooking Time: {meal.readyInMinutes} min</div>
-        <div>Servings: {meal.servings}</div>
-      </div>
-      <div className="ratingContainer"> {rating ? <div className="ratings"> <Rating rating={rating.avgRating} /> <div className="ratingCount"> ({rating.numRatings}) </div> </div>: <p>Loading...</p>} </div>
-    </div>
+    // <div className='meal-card' onClick={handleCardClick}>
+    //   <img src={meal.image}/>
+    //   <div className='meal-card-content'>
+    //     <div className='title'>{meal.title}</div>
+    //     <div>Cooking Time: {meal.readyInMinutes} min</div>
+    //     <div>Servings: {meal.servings}</div>
+    //   </div>
+    //   <div className="ratingContainer"> 
+    //        {rating ? <div className="ratings"> 
+    //           <Rating rating={rating.avgRating} /> 
+    //           <div className="ratingCount"> ({rating.numRatings}) </div> 
+    //    </div>: <p>Loading...</p>} </div>
+    // </div>
+
+   
+    <Card
+     border='dark'
+     className='mb-2'
+     text="dark"
+     >
+      <Card.Img variant="top" src={meal.image} alt={meal.title} />
+        <Card.Body>
+          <Card.Title style ={{fontFamily: 'Roboto'}}>
+            <b>{meal.title}</b>
+          </Card.Title>
+          <Card.Text>
+            Cooking Time: {meal.readyInMinutes} min
+          </Card.Text>
+          <Card.Text>
+            Servings: {meal.servings}
+          </Card.Text>
+          <Card.Footer>
+            {rating ? <div className="ratings"> 
+              <Rating rating={rating.avgRating} /> 
+               <div className="ratingCount"> ({rating.numRatings}) </div> 
+          </div>: <p>Loading...</p>}
+          </Card.Footer>
+        </Card.Body>
+    </Card>
   )
 }
 
